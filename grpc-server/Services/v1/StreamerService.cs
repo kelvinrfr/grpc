f82ecbf62c0a2308191c0eb37f6a78a7f89b0c85
kelvinrferreira <10376375+kelvinrferreira@users.noreply.github.com>
2020-08-02 21:37:31 -0300

    adding streamer service to server

---
+++ grpc-server/Services/v1/StreamerService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace grpc_server.v1
+{
+    public class StreamerService: Streamer.StreamerBase
+    {
+        /// <summary>
+        /// Writing 10 numbers through a stream with an interval of 1 second
+        /// on each message.
+        /// </summary>
+        /// <param name="request">The received object containing the seed</param>
+        /// <param name="responseStream">The response stream used to reply the message</param>
+        /// <param name="context">Server call context</param>
+        /// <returns>Task</returns>
+        public async override Task GetDataAsync(
+            GetDataRequest request,
+            IServerStreamWriter<GetDataReply> responseStream,
+            ServerCallContext context)
+        {
+            var random = new Random(request.Seed);
+
+            for(int i = 0; i< 10; i++)
+            {
+                await responseStream.WriteAsync(new GetDataReply
+                {
+                    Number = random.Next(),
+                    Datetime = DateTime.UtcNow.ToString()
+                });
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+        }
+    }
+}
